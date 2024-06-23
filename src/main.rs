5c5f41dad5516ad82aaf11736eff050c601db49c
Rasmus Larsen <doctorherbstein@gmail.com>
2024-06-24 01:38:37 +0200

    Simple working class file reader

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use cgr::ClassFile;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file = std::env::args()
+        .nth(1)
+        .expect("First argument is input class file");
+    let file = std::fs::read(file)?;
+    let file = match ClassFile::read(&file) {
+        Ok((_, file)) => file,
+        Err(err) => return Err(err.to_owned().into()),
+    };
+
+    println!("{file:#?}");
+
+    Ok(())
+}
